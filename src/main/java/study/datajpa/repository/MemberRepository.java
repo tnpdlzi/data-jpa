96d158d9f4f2886f5088008863e7afe298885220
Lee Donggun <tnpdlzi@naver.com>
2021-07-30 17:32:10 +0900

    feat: @Query를 이용해 리포지토리 메소드에 쿼리 정의하기

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -14,6 +14,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     // 메소드 이름으로 쿼리를 생성해준다. 관례를 가지고.
     // Username과 AndAge하면 and 조건으로 묶인다. GreaterThan하면 이 파라미터 조건보다 크면! 으로 나오게 된다.
     // 이름을 다르게 하면 안 된다. 프로퍼티 명을 맞춰줘야한다.
+    // 길어지면 문제가 생기는 단점.
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
     List<Member> findTop3HelloBy();
@@ -21,4 +22,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     // @Param은 :username과 같이 명확히 jpql 이 적혀있을 때 필요하다.
 //    @Query(name = "Member.findByUsername") // jpql을 이걸로 찾아서 실행해준다. 근데 이걸 없애도 동작한다. 관례가 있기 때문. 해당 타입에 .을 찍고 메소드 이름으로 named query를 찾는다.
     List<Member> findByUsername(@Param("username") String username);
+
+    // 실무에서 많이 사용하는 방법.
+    // 여기에 바로 쿼리를 쳐버릴 수 있다. JPQL을 인터페이스 메서드에 바로 적어버릴 수 있다는 장점이 있다.
+    // 또한 오타를 쳐도 애플리케이션 로딩 시점에 문법 오류를 잡아준다는 장점이 있다.
+    // 동적 쿼리는 Query Dsl을 쓰자.
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
