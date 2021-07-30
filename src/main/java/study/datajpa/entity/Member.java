4383f1f1edf04475149f179e8fa5d0d4407f4fa4
Lee Donggun <tnpdlzi@naver.com>
2021-07-30 12:43:57 +0900

    feat: 순수 JPA기반 리포지토리 만들기

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,31 +1,51 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"}) //team은 들어가면 안된다. 그럼 무한 루프 일어남. 연관관계 필드는 toString 하면 안 된다.
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long Id;
     private String username;
+    private int age;
 
-    // jpa의 프록시 기술에서 객체를 만들어야 할 때 필요. private은 안 됨.
-    protected Member() {
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+//    // jpa의 프록시 기술에서 객체를 만들어야 할 때 필요. private은 안 됨. 이거 대신 noargsconstructor 해서 protected 하면 된다.
+//    protected Member() {
+//    }
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
     }
 
 //    // setter 말고 이런 식으로 하는 게 더 나은 방법이다.
 //    public void changeUsername(String username) {
 //        this.username = username;
 //    }
+
+    // == 연관관계 편의 메서드 == //
+    // 팀에도 멤버가 추가되도록.
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
 }
--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
@@ -26,5 +28,39 @@ class MemberJpaRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        // 단건 조회 검증.
+        // 사실 get을 통해 꺼내는 것은 안 좋은 방법이다.
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        // 변경 감지 (더티체킹)를 이용한 변경. 그래서 변경 메서드가 따로 없다.
+//        findMember1.setUsername("updated member");
+        
+        // 리스트 조회 검증
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        // 삭제 검증
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long deletedCount = memberJpaRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
+
     }
 }
